12ad2daa309da181abf50e6649496ecfd1c1d44e
Dandelea <dandelea@alum.us.es>
2015-08-28 14:13:33 +0200

    Version1.01

--- core/src/tennis/SpaceTennis3D.java
+++ core/src/tennis/SpaceTennis3D.java
@@ -19,7 +19,7 @@ import com.badlogic.gdx.Screen;
  */
 public class SpaceTennis3D extends Game {
 	public static final String TITLE = "Space Tennis 3D";
-	public static final String VERSION = "ver 0.1";
+	public static final String VERSION = "1.01";
 	public static int WIDTH;
 	public static int HEIGHT;
 	public static Scoreboard lastScoreboard;
